2287045ce9e40bab0bf9f8391609c53984e13cbe
Archer <ogunkapc@gmail.com>
2023-05-10 17:11:33 +0100

    updated mainActivity

--- androidApp/src/main/java/com/example/spacexlaunches/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/spacexlaunches/android/MainActivity.kt
@@ -1,40 +1,79 @@
 package com.example.spacexlaunches.android
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.spacexlaunches.Greeting
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.example.spacexlaunches.shared.entity.RocketLaunch
 
-class MainActivity : ComponentActivity() {
+//class MainActivity : ComponentActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContent {
+//            MyApplicationTheme {
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colors.background
+//                ) {
+//                    GreetingView(Greeting().greet())
+//                }
+//            }
+//        }
+//    }
+//}
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var launchesRecyclerView: RecyclerView
+    private lateinit var progressBarView: FrameLayout
+    private lateinit var swipeRefreshLayout: SwipeRefreshLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MyApplicationTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    GreetingView(Greeting().greet())
-                }
-            }
+
+        title = "SpaceX Launches"
+        setContentView(R.layout.activity_main)
+
+        launchesRecyclerView = findViewById(R.id.launchesListRv)
+        progressBarView = findViewById(R.id.progressBar)
+        swipeRefreshLayout = findViewById(R.id.swipeContainer)
+    }
+}
+
+class LaunchesRvAdapter(var launches: List<RocketLaunch>) : RecyclerView.Adapter<LaunchesRvAdapter.LaunchViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LaunchViewHolder {
+        return LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_launch, parent, false)
+            .run(::LaunchViewHolder)
+    }
+
+    override fun getItemCount(): Int = launches.count()
+
+    override fun onBindViewHolder(holder: LaunchViewHolder, position: Int) {
+        holder.bindData(launches[position])
+    }
+
+    inner class LaunchViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        // ...
+        fun bindData(launch: RocketLaunch) {
+            // ...
         }
     }
 }
 
-@Composable
-fun GreetingView(text: String) {
-    Text(text = text)
-}
-
-@Preview
-@Composable
-fun DefaultPreview() {
-    MyApplicationTheme {
-        GreetingView("Hello, Android!")
-    }
-}
+//@Composable
+//fun GreetingView(text: String) {
+//    Text(text = text)
+//}
+//
+//@Preview
+//@Composable
+//fun DefaultPreview() {
+//    MyApplicationTheme {
+//        GreetingView("Hello, Android!")
+//    }
+//}
