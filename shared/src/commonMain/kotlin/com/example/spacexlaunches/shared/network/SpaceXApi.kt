50f9d67a3adbf290f332bc2828ccb6d366b1939b
Archer <ogunkapc@gmail.com>
2023-05-10 16:17:11 +0100

    implement API service

---
+++ shared/src/commonMain/kotlin/com/example/spacexlaunches/shared/network/SpaceXApi.kt
@@ -0,0 +1,26 @@
+package com.example.spacexlaunches.shared.network
+
+import com.example.spacexlaunches.shared.entity.RocketLaunch
+import io.ktor.client.*
+import io.ktor.client.call.body
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.get
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
+
+class SpaceXApi {
+    private val httpClient = HttpClient {
+        install(ContentNegotiation) {
+            json(
+                Json {
+                    ignoreUnknownKeys = true
+                    useAlternativeNames = false
+                }
+            )
+        }
+    }
+
+    suspend fun getAllLaunches(): List<RocketLaunch> {
+        return httpClient.get("https://api.spacexdata.com/v5/launches").body()
+    }
+}
